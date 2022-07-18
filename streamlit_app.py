65f64f0865c0ff917787f8c4e05df3178f2fd7f7
Neelesh1983 <109515400+Neelesh1983@users.noreply.github.com>
2022-07-18 15:17:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,3 +21,16 @@ st.time_input('School time')
 st.text_area('Description')
 st.file_uploader('Upload a photo')
 st.color_picker('Choose your favorite color')
+st.balloons()
+st.progress(10)
+with st.spinner('Wait for it...'):
+    time.sleep(10)
+    
+st.success("You did it !")
+st.error("Error")
+st.warnig("Warning")
+st.info("It's easy to build a streamlit app")
+st.exception(RuntimeError("RuntimeError exception"))
+## sidebar, spinner, echo 
+st.sidebar.title('This is sidebar')
+st.sidebar.button('click')
