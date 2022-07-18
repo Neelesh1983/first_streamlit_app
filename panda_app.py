25500136e02f05d39e70514a9e3cd71ab5209855
Neelesh1983 <109515400+Neelesh1983@users.noreply.github.com>
2022-07-18 21:34:45 +0530

    Update panda_app.py

--- panda_app.py
+++ panda_app.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import streamlit as st
 st.title('My Neighbours favourite daily')
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+st.header('My Favourite Breakfast')
 st.text('🥣 Omega 3 & Blueberry Oatmeal')
 st.text('🥗 Kale, Spinach & Rocket Smoothie')
 st.text('🐔 Hard-Boiled Free-Range Egg')
 st.text('🥑🍞 Avocado Toast')
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 st.dataframe(my_fruit_list)
