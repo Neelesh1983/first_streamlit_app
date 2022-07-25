622df0c2af305dd88de19746f129cc96db7f9cdf
Neelesh1983 <109515400+Neelesh1983@users.noreply.github.com>
2022-07-25 15:38:05 +0530

    Update panda_app.py

--- panda_app.py
+++ panda_app.py
@@ -9,6 +9,6 @@ st.text('🥑🍞 Avocado Toast')
 st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_selected = st.multiselect('Pick some fruits:',list(my_fruit_list.index),['Avocado','Strawberries'])
+fruits_selected = st.multiselect('Pick some fruits:',list(my_fruit_list.index))
 Fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(Fruits_to_show)
