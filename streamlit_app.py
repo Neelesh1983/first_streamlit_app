a2b4a662e0c4c55c5e9a8fab4dce32eaa51863dd
Neelesh1983 <109515400+Neelesh1983@users.noreply.github.com>
2022-08-17 15:18:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,11 +33,11 @@ st.progress(10)
 ## with st.spinner('Wait for it...'):
 ##   time.sleep(10)
     
-st.success("You did it !")
-st.error("Error")
-st.warning("Warning")
-st.info("It's easy to build a streamlit app")
-st.exception(RuntimeError("RuntimeError exception"))
+#st.success("You did it !")
+#st.error("Error")
+#st.warning("Warning")
+#st.info("It's easy to build a streamlit app")
+#st.exception(RuntimeError("RuntimeError exception"))
 
 ## sidebar, spinner, echo 
 
