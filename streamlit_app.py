f6832d5e42572ade279d3fe91701bf31dccb3c11
Neelesh1983 <109515400+Neelesh1983@users.noreply.github.com>
2022-08-17 13:56:33 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,6 +44,12 @@ st.exception(RuntimeError("RuntimeError exception"))
 st.sidebar.title('This is sidebar')
 st.sidebar.button('click')
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
 
     
 
